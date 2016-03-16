2e543b07627a9114a580db50600b4f9902dda080
crystalDf <chendong333@gmail.com>
2016-03-16 21:18:27 +0800

    Chapter 28 PhotoGallery

--- app/src/main/java/com/star/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/star/photogallery/PhotoPageFragment.java
@@ -4,11 +4,14 @@ package com.star.photogallery;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class PhotoPageFragment extends VisibleFragment {
 
@@ -16,6 +19,7 @@ public class PhotoPageFragment extends VisibleFragment {
 
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static PhotoPageFragment newInstance(Uri uri) {
         Bundle args = new Bundle();
@@ -40,9 +44,29 @@ public class PhotoPageFragment extends VisibleFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_page, container, false);
 
+        mProgressBar = (ProgressBar) view.findViewById(R.id.fragment_photo_page_progress_bar);
+        mProgressBar.setMax(100);
+
         mWebView = (WebView) view.findViewById(R.id.fragment_photo_page_web_view);
 
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                AppCompatActivity appCompatActivity = (AppCompatActivity) getActivity();
+                appCompatActivity.getSupportActionBar().setSubtitle(title);
+            }
+        });
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
